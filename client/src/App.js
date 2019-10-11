25ddaba610c7187472f530b2516a3bef0f0b1885
unknown <alexander.norberghoff@gmail.com>
2019-10-11 19:59:13 +0200

    electron prompt work around

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Namespace from './components/Namespace';
 const App = () => {
+  if (!window.prompt) {
+
+  }
   if (!localStorage.getItem('name')) {
+    // The try catch is for Electron.
+    // It does not support prompt.
     try {
       localStorage.setItem('name', window.prompt('Username: '))
     } catch {
-      localStorage.setItem('name', 'Jasmine')
+      localStorage.setItem('name', 'test')
     }
   }
   return (
